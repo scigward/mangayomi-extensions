bf53fb448fe48880ac267ca75644f7b1a0be3712
scigward <162128369+scigward@users.noreply.github.com>
2025-03-23 22:40:17 +0300

    Update source.dart

--- dart/anime/src/ar/okanime/source.dart
+++ dart/anime/src/ar/okanime/source.dart
@@ -3,10 +3,10 @@ import '../../../../../model/source.dart';
 Source get okanimeSource => _okanimeSource;
 const _okanimeVersion = "0.0.55";
 const _okanimeSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/ar/okanime/okanime.dart";
+    "https://raw.githubusercontent.com/scigward/mangayomi-extensions/refs/heads/main/dart/anime/src/ar/okanime/okanime.dart";
 Source _okanimeSource = Source(
   name: "Okanime",
-  baseUrl: "https://www.okanime.xyz",
+  baseUrl: "https://okanime.tv/",
   lang: "ar",
   typeSource: "single",
   iconUrl:
